6be056246004ae04ee1a8107cf4a26343ee37561
nicola-calonaci <nicola.calonaci@gmail.com>
2025-07-30 13:01:19 +0200

    initialise package

---
+++ R/download_data.R
@@ -0,0 +1,13 @@
+#' Classify mutations using a Beta-Binomial model-based test.
+#'
+#' @param
+#' @return
+#' @export
+#' @examples
+#' print(x)
+#' @importFrom dplyr filter mutate rename select %>%
+
+download_data = function(study = "msk_chord_2024"){
+
+  msk_chord = cBioPortalData::cBioDataPack(study, ask = FALSE)
+}

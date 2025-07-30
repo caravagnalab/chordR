991a0add84550f11e66e27deeadb95370e7c2e87
Andrea Buscema <andrea.buscema@outlook.it>
2025-07-30 19:01:03 +0200

    other fix: removed untitled, commented some test sscripts, fixed description

--- R/subset_dataset.R
+++ R/subset_dataset.R
@@ -10,15 +10,15 @@ subset_data = function(x){
   msk_chord = cBioPortalData::cBioDataPack(study, ask = FALSE)
 }
 
-xx = subsetByColData(x = msk_chord, y = "P-0002869")
-xx@ExperimentList@listData$mutations@assays %>% names()
-
-msk_chord@ExperimentList@listData$mutations@assays %>% class()
-
-yy = subsetByColumn(x = msk_chord, y = "P-0002869")
-yy@ExperimentList@listData$mutations@assays %>% as_tibble()
-
-zz = subsetByRow(x = msk_chord, y = "P-0002869")
-zz@ExperimentList@listData$mutations@assays %>% as_tibble()
-
-colData(x = msk_chord) %>% colnames()
+# xx = subsetByColData(x = msk_chord, y = "P-0002869")
+# xx@ExperimentList@listData$mutations@assays %>% names()
+# 
+# msk_chord@ExperimentList@listData$mutations@assays %>% class()
+# 
+# yy = subsetByColumn(x = msk_chord, y = "P-0002869")
+# yy@ExperimentList@listData$mutations@assays %>% as_tibble()
+# 
+# zz = subsetByRow(x = msk_chord, y = "P-0002869")
+# zz@ExperimentList@listData$mutations@assays %>% as_tibble()
+# 
+# colData(x = msk_chord) %>% colnames()
